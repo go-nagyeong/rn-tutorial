8db493eb5184855f6f8ddf8033b7c1593cfc962c
go-nagyeong <ngkim0929@naver.com>
2021-12-05 03:43:49 +0900

    feat: :sparkles: Update main component

--- Todolist/App.js
+++ Todolist/App.js
@@ -6,107 +6,88 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, {useState} from 'react';
+ import {
+   SafeAreaView,
+   StyleSheet,
+   TextInput,
+   View,
+   Text,
+   ScrollView,
+ } from 'react-native';
+ import TodoInsert from './components/TodoInsert';
+ import TodoList from './components/TodoList';
+ 
+ const App = () => {
+   const [todos, setTodos] = useState([]);
+   
+   const addTodo = text => {
+     setTodos([
+       ...todos,  // ... 연산자 : Rest Parameter? Spread Operator?
+       {id: Math.random().toString(), textValue: text, checked: false},
+     ]);
+   };
+ 
+   // 할 일 목록 삭제하는 함수
+   const onRemove = id => e => {
+     setTodos(todos.filter(todo => todo.id !== id));
+   };
+ 
+   // 체크 박스 표시/해제하는 함수
+   const onToggle = id => e => {
+     setTodos(
+       todos.map(todo =>
+         todo.id === id ? {...todo, checked: !todo.checked} : todo,
+         // ? 연산자 : 콜론기호(:) 와 함께 삼항 연산자로 사용
+         // <조건 ? 참일때의 결과 : 거짓일때의 결과>
+       ),
+     );
+   };
+ 
+   return (
+     // SafeAreaView를 사용하면 아이폰의 노치 영역을 감안하여 자동으로 패딩 계산함
+     // (콘텐츠가 노치에 가려지지 않게 하기 위함)
+     <SafeAreaView style={styles.container}>
+       <Text style={styles.appTitle}>Hello Todolist</Text>
+       <View style={styles.card}>
+         {/* addTodo 함수를 TodoInsert 컴포넌트로 전달 */}
+         <TodoInsert onAddTodo={addTodo} />
+         {/* 할 일 목록의 객체가 담긴 배열과 함수들을 TodoList와 TodoItem 컴포넌트에 전달 */}
+         <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+       </View>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#3143e8',
+   },
+   appTitle: {
+     color: '#fff',
+     fontSize: 36,
+     marginTop: 30,
+     marginBottom: 30,
+     fontWeight: '300',
+     textAlign: 'center',
+     backgroundColor: '#3143e8',
+   },
+   card: {
+     backgroundColor: '#fff',
+     flex: 1,
+     borderTopLeftRadius: 10,
+     borderTopRightRadius: 10,
+     marginLeft: 10,
+     marginRight: 10,
+   },
+   input: {
+     padding: 20,
+     borderBottomColor: '#bbb',
+     borderBottomWidth: 1,
+     fontSize: 24,
+     marginLeft: 20,
+   },
+ });
+ 
+ export default App;
