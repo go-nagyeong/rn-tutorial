3533279cbcb10661bce4d3f10a9c432575f5e3b6
go-nagyeong <ngkim0929@naver.com>
2021-12-05 04:03:33 +0900

    feat: :sparkles: Update main component

--- Navigation/StackNavigation/App.js
+++ Navigation/StackNavigation/App.js
@@ -6,107 +6,88 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, {Component} from 'react';
+ import {Text, StyleSheet, View, Button} from 'react-native';
+ import { createAppContainer } from 'react-navigation';
+ // createAppContainer: Navigation으로 만들어낸 Stack 들을 하나의 상자에 담아 앱이 인식 할 수 있는 형태로 만들어주는 API
+ import { createStackNavigator } from 'react-navigation-stack';
+ 
+ // 클래스형 컴포넌트 (Todolist의 경우, 함수형 컴포넌트)
+ class Home extends Component {
+   render() {
+     return (
+       <View style={styles.eachView}>
+         <Text>홈 화면 입니다.</Text>
+         <Button
+           title="챗 화면으로 가기"
+           onPress={() => this.props.navigation.navigate('Chat')} />
+           {/* 
+           1. props: 변하지 않는 데이터 / 상위 컴포넌트에서 하위 컴포넌트로 데이터를 넘겨줄 때 사용
+             => <컴포넌트이름 props이름 = “값”>
+                이렇게 상위 컴포넌트에서 HTML의 attribute를 정의하듯이 하위 컴포넌트의 속성처럼 사용하면 됨
+           2. navigation.navigate(name, params): route(name)에 parameter(params)를 전달
+           */}
+         <Button
+           title="세팅 화면으로 가기"
+           onPress={() => this.props.navigation.navigate('Settings')} />
+       </View>
+     )
+   }
+ }
+ 
+ class Chat extends Component {
+   render() {
+     return (
+       <View style={styles.eachView}>
+         <Text>챗 화면 입니다.</Text>
+         <Button
+           title="홈 화면으로 가기"
+           onPress={() => this.props.navigation.navigate('Home')} />
+       </View>
+     )
+   }
+ }
+ 
+ class Settings extends Component {
+   render() {
+     return (
+       <View style={styles.eachView}>
+         <Text>세팅 화면 입니다.</Text>
+         <Button
+           title="홈 화면으로 가기"
+           onPress={() => this.props.navigation.navigate('Home')} />
+       </View>
+     )
+   }
+ }
+ 
+ const App = createStackNavigator(
+   {
+     Chat: {screen: Chat},
+     Home: {screen: Home},
+     Settings  // 설정이 'screen' 밖에 없다면, 중괄호는 생략 가능
+   },
+   { 
+     // 처음 보여 줄 화면 설정
+     initialRouteName: 'Home'
+   }
+ );
+ 
+ const AppContainer = createAppContainer(App);
+ 
+ export default () => (
+   <AppContainer />
+ );
+ 
+ const styles = StyleSheet.create({
+   centerView: {
+     flex: 1,
+     justifyContent: "center",
+     alignItems: "center"
+   },
+   eachView: {
+     flex: 1,
+     justifyContent: "center",
+     alignItems: "center"
+   }
+ });
